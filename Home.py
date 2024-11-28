49646300227a1d2a9785b6a2187a68be7acb553e
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-11-28 17:57:18 +0630

    feat: update

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import streamlit as st
 from tools import llm_generate
 from manager import manager
 import streamlit.components.v1 as components
+import base64
 
 st.set_page_config(page_title="Shark Label", page_icon="🦈", layout="wide")
 big_title = st.empty()
@@ -67,24 +68,35 @@ elif selected_menu == APP_MENU:
     st.markdown(f"{len(manager.data)} images waiting to be labelled.")
 
     if st.button("Label", icon="➡️"):
-        image_url = manager.load_image(st.session_state["current_user_email"])[
-            "options"
-        ]["backgroundImage"]["url"]
+        loaded_image = manager.load_image(st.session_state["current_user_email"])["url"]
 
-        with st.spinner("Downloading image"):
-            resp = requests.get(image_url)
-
+        if isinstance(loaded_image, bytes):
+            image = loaded_image
+            image_url = f"data:image/png;base64,{base64.b64encode(loaded_image).decode('utf-8')}"
+        else:
+            with st.spinner("Downloading image"):
+                resp = requests.get(loaded_image)
+            image = resp.content
+            image_url = loaded_image
+        
         with st.spinner("Generaing a.i. descriptors"):
             _, response = llm_generate(
-                image=resp.content,
+                image=image,
                 prompt="I need to train an AI model to segment lines properly on geometry questions. Here is a question, give me 10 text prompts that refer to lines in an image so I can label them for my training data.",
             )
 
-        descriptors = [d for d in response.split("\n") if d]
+        def clean_descriptor(t: str):
+            return t.replace('"', "")[2:]
+
+        descriptors = [clean_descriptor(d) for d in response.split("\n") if d]
+        descriptors = descriptors[1:]  # skip the first preface
+        st.markdown("**Please scroll down to see all the descriptors**")
         for i, d in enumerate(descriptors):
-            st.text_input(label=f"Label {i}/{(len(descriptors))}", value=d)
-            placeholder = st.text(f"Data {image_url}")
-            components.html(manager.labeler.replace("{image_url}", image_url), height=500)
+            label = st.text_input(label=f"Label {i}/{(len(descriptors))}", value=d)
+            components.html(
+                manager.labeler.replace("{image_url}", image_url).replace("{label}", label),
+                height=500,
+            )
 
 
 elif DATA_MENU:
--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 import os
+import base64
 import json
 from tools import get_image_items
 from itertools import chain
@@ -6,9 +7,10 @@ from itertools import chain
 
 class Manager:
     def __init__(self) -> None:
+        print("Initializing manager...")
         # Load data
-        with open("khan-academy-data.json") as f:
-            data = json.load(f)
+        with open("khan-academy-data.json") as fp:
+            data = json.load(fp)
 
         khanacademy = [
             {**img, "url": img["options"]["backgroundImage"]["url"]}
@@ -17,7 +19,14 @@ class Manager:
             and img["options"]["backgroundImage"]["url"].endswith(".png")
         ]
         cracksat = []
-        self.data = [*khanacademy]
+        for subdirs, _, files in os.walk("cracksat-images"):
+            for fp in files:
+                with open(f"cracksat-images/{fp}", "rb") as f:
+                    cracksat.append(
+                        {"id": fp, "url": f.read()}
+                    )
+
+        self.data = [*khanacademy, *cracksat]
 
         self.registeration = {}
         self.load()
@@ -25,8 +34,9 @@ class Manager:
         work_done = list(chain.from_iterable(self.registeration.values()))
         self.data = [d for d in self.data if d not in work_done]
 
-        with open("labeler.html", "r") as f:
-            self.labeler = f.read()
+        with open("labeler.html", "r") as fp:
+            self.labeler = fp.read()
+        print("Manager initialized...")
 
     def load(self):
         if not os.path.exists("work.json"):
@@ -41,7 +51,7 @@ class Manager:
             self.registeration[email] = []
 
         img = self.data.pop()
-        self.registeration[email].append(img)
+        self.registeration[email].append(img["id"])
         self.save()
         return img
 
